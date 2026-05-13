c72aa218fdc9041ffb401107fdea85663ce21274
hantie <262029@virtualwindow.co.za>
2026-05-13 19:56:21 +0200

    Added contact modal and JS counter buttons
    Added modal for contact form submission
    Added javascript functionality for counter buttons
     - Adjusted css styling to remove arrows/spinboxes for all numeric inputs
    
    Javascript script file is acting a bit weird on pages that don't have counter/contant form. I'll look into that still

--- admin/ReferencesAndTutorials.md
+++ admin/ReferencesAndTutorials.md
@@ -6,4 +6,8 @@
 
 ### Modal/ pop up message on thank you
 
-- https://youtu.be/XH5OW46yO8I
+- https://youtu.be/XH5OW46yO8I <!-- Not using this but rather using bootstrap -->
+
+### Removing arrows/spinboxes for all numeric inputs
+
+- https://www.w3schools.com/howto/howto_css_hide_arrow_number.asp
--- js/script.js
+++ js/script.js
@@ -1,13 +1,71 @@
+// Form Time !!
+// Get Form data
 
-// Modal Pop up
-const submit = document.getElementById('submit');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+let form = document.forms['filledForm'];
+form.addEventListener('submit', getFormInfo);
 
-submit.addEventListener('click', () => {
-    modal_container.classList.add('show');
-})
+// Extract the Form data into an object
+function getFormInfo(event){
+    // Denying of placeholder text
+    event.preventDefault();
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-})
+    // Create the object that will store the form data
+    let formData = {
+        'name' : this['name'].value,
+        'email' : this['email'].value,
+        'subject' : this['subject'].value,
+        'message' : this['message'].value
+    };
+
+    // Write the HTML to the variable to be interpolated
+    let output = `
+        <p>
+            Thank you, <span>${formData['name']}</span> for your message!
+        </p>
+        <p>
+            We will contact you soon
+        </p>
+    `;
+
+    // Select the HTML Section to update
+    let outSection = document.querySelector('.outputContainer');
+
+
+    // Get and populate a Modal from bootstrap and show
+    // Populating the section of the bootstrap modal
+    outSection.innerHTML = output;
+
+    // Get corresponding HTML element
+    let formModal = new bootstrap.Modal(
+        document.getElementById('formModal')
+    );
+
+    // Show Modal
+    formModal.show();
+
+    // Clear Form
+    form.reset();
+};
+
+
+
+// Counter
+// Using a singular function to do the counting,
+// I feel like the should be a more efficient way of doing it tho
+countMe = (operator, petID) => {
+    // Extract element from HTML
+    let num = document.getElementById(petID).value;
+
+    if (operator == 'minus' ){
+        if (num > 0){
+            num -=1;
+        }
+    };
+
+    if (operator == 'plus' ){
+        if (num < 50){
+            num ++ ;
+        }
+    };
+    document.getElementById(petID).value = num;
+};
